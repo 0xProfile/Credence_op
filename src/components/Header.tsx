62b9ea643ad861fc01c15c0d2660be499e053ccd
Kehan Wang <wangkk@bu.edu>
2023-03-17 18:19:19 -0400

    Add a logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 import React from "react";
 import { updateSearchValue } from "../action/search";
 import { useDispatch } from 'react-redux'
+import logo from '../assets/header-logo2.png';
 
 
 export const Header = () => {
@@ -10,7 +11,9 @@ export const Header = () => {
   return (
     <div className="navbar bg-base-100 shadow rounded-box">
       <div className="flex">
-        <a className="btn btn-ghost normal-case text-xl">Some Logo?</a>
+      <a className="btn btn-ghost normal-case text-xl">
+        <img src={logo} alt="Image" className="object-contain w-full h-full" />
+      </a>
       </div>
       <div className="flex-1 gap-2">
         <div className="form-control  w-full px-2">
